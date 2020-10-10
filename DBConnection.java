17c290e703112bdce11cc6aadc446249a68c8dfb
Siddhantpandey19 <71272632+Siddhantpandey19@users.noreply.github.com>
2020-10-10 12:40:26 +0530

    Add files via upload

---
+++ DBConnection.java
@@ -0,0 +1,45 @@
+package com.hsbc.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBConnection {
+	Connection con;
+	Statement st;
+	PreparedStatement pst;
+
+	public DBConnection() {
+		try {
+			Class.forName("org.apache.derby.jdbc.ClientDriver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public Statement getStatement() {
+		try {
+			con = DriverManager.getConnection("jdbc:derby://localhost:1527/MyDBTest;");
+			st = con.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return st;
+	}
+
+	public PreparedStatement getPreparedStatement(String sql) {
+		try {
+			con = DriverManager.getConnection("jdbc:derby://localhost:1527/MyDBTest;");
+			pst = con.prepareStatement(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return pst;
+	}
+
+}
